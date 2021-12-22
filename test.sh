ff49078e5630d21f81b7d669ae8f14f01bd70d07
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-12-22 18:15:48 -0500

    Avoid nvmrc usage.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-nvm install
+nvm install v14
 npm install --global npm@7.x
 
 # Debug output
@@ -10,7 +10,7 @@ node --version
 npm --version
 npm run check-path
 
-# Run outside npm run-script
+# Run outside npm run-script: Should work!
 ./bin/test-env-node
-# Run inside npm run-script
+# Run inside npm run-script: Should FAIL!
 npm run test
