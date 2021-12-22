2823e1c7fdf0ed63e65112f827b0b349da2c7c96
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-12-22 18:06:51 -0500

    Playing with npm version.

--- test.sh
+++ test.sh
@@ -3,5 +3,14 @@
 set -euo pipefail
 
 nvm install
-npm run test
+npm install --global npm@6.x
+
+# Debug output
+node --version
+npm --version
 npm run check-path
+
+# Run outside npm run-script
+./bin/test-env-node
+# Run inside npm run-script
+npm run test
