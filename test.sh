f49dad3c82ef6ae0bbbaf38920d2b64796254f55
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-12-22 18:22:23 -0500

    Full test case almost done!

--- test.sh
+++ test.sh
@@ -12,5 +12,17 @@ npm run check-path
 
 # Run outside npm run-script: Should work!
 ./bin/test-env-node
+# Run inside npm run-script: Should work!
+npm run test
+
+npm install --global npm@7.0.0
+
+# Debug output
+node --version
+npm --version
+npm run check-path
+
+# Run outside npm run-script: Should work!
+./bin/test-env-node
 # Run inside npm run-script: Should FAIL!
 npm run test
