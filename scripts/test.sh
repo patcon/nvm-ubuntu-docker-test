6f857308be57cef0b2cefd8ab77a7a622ac3f606
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-12-22 16:26:30 -0500

    Minimizing more.

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,11 +2,7 @@
 
 set -euo pipefail
 
-#setup nvm
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
 cd /var/www/html/app
-nvm install --latest-npm
+nvm install
 npm run test
 npm run check-path
