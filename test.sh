c47ed3e1a1e59bb1e4a7d67ada1ff74022fc7c68
Patrick Connolly <patrick.c.connolly@gmail.com>
2021-12-26 19:08:58 -0500

    Better output. Trying to find beta where 7.0.0 starts failing.

--- test.sh
+++ test.sh
@@ -2,8 +2,11 @@
 
 set -euo pipefail
 
+WORKING_NODE_VERSION=6.x
+FAILING_NODE_VERSION=7.0.0
+
 nvm install --no-progress v14
-npm install --global npm@6.x
+npm install --global npm@${WORKING_NODE_VERSION}
 
 # Debug output
 node --version
@@ -17,7 +20,7 @@ echo '--- This will work!'
 echo '--- This will work!'
 npm run test
 
-npm install --global npm@7.0.0
+npm install --global npm@${FAILING_NODE_VERSION}
 
 # Debug output
 node --version
@@ -28,6 +31,7 @@ npm run check-path
 echo '--- This will work!'
 ./bin/test-env-node
 # Run inside npm run-script.
-echo '--- This will WORK on debian!'
-echo '--- This will FAIL on ubuntu!'
+echo '--- This will WORK on official container: node!'
+echo '--- This will FAIL on official container: debian!'
+echo '--- This will FAIL on official container: ubuntu!'
 npm run test
